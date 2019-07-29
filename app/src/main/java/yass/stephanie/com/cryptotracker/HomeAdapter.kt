bf6d13784a957a9e186db75476c2f56d4307209d
thedevsteph <thedevsteph@gmail.com>
2019-07-29 16:06:01 +0100

    populating ui with mock data

--- app/src/main/java/yass/stephanie/com/cryptotracker/HomeAdapter.kt
+++ app/src/main/java/yass/stephanie/com/cryptotracker/HomeAdapter.kt
@@ -4,7 +4,6 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
 
 class HomeAdapter(private val data: ArrayList<Currencies>) : RecyclerView.Adapter<HomeAdapter.HomeViewHolder>() {
@@ -14,7 +13,7 @@ class HomeAdapter(private val data: ArrayList<Currencies>) : RecyclerView.Adapte
         val currencyLongText: TextView = view.findViewById(R.id.currency_long_text)
         val currencyShortText: TextView = view.findViewById(R.id.currency_short_text)
         val percentagePriceChange: TextView = view.findViewById(R.id.percentage_price_change)
-        var currencyIcon: ImageView = view.findViewById(R.id.currency_icon)
+        val currentHoldingValue: TextView = view.findViewById(R.id.current_holding_value)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeAdapter.HomeViewHolder {
@@ -26,12 +25,26 @@ class HomeAdapter(private val data: ArrayList<Currencies>) : RecyclerView.Adapte
         holder.currencyLongText.text = data[position].longName
         holder.currencyShortText.text = data[position].abbreviatedName
         holder.percentagePriceChange.text = "45.40%"
-        holder.currencyIcon.background = holder.view.context.getDrawable(R.drawable.abc_ab_share_pack_mtrl_alpha)
+        holder.currentHoldingValue.text = "£${data[position].currentPrice}"
+
+        //change the background - temporary code
+        when {
+            position % 2 == 0 -> stringProvider(holder, R.color.abc_btn_colored_borderless_text_material)
+            position % 5 == 0 -> stringProvider(holder, R.color.error_color_material_light)
+            else -> null
+        }
+
     }
 
     override fun getItemCount(): Int {
         return data.size
     }
+
+    private fun stringProvider(holder: HomeViewHolder, colorInt: Int){
+        val color = holder.itemView.context.getColor(colorInt)
+        holder.percentagePriceChange.setTextColor(color)
+    }
+
 
 
 }
--- app/src/main/java/yass/stephanie/com/cryptotracker/MainActivity.kt
+++ app/src/main/java/yass/stephanie/com/cryptotracker/MainActivity.kt
@@ -7,8 +7,9 @@ import android.support.v7.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
-    val ethereum: Currencies = Currencies("ETH", "34.344", R.drawable.abc_ab_share_pack_mtrl_alpha, "Ethereum")
-    val fakeData = arrayListOf(ethereum, ethereum, ethereum)
+    val ethereum: Currencies = Currencies("ETH", "34.34", R.drawable.abc_ab_share_pack_mtrl_alpha, "Ethereum")
+    val bitcoin: Currencies = Currencies("BTC", "31,113.44", R.drawable.abc_ab_share_pack_mtrl_alpha, "Bitcoin")
+    val fakeData = arrayListOf(ethereum, bitcoin, ethereum, ethereum, bitcoin, bitcoin, ethereum, ethereum, bitcoin)
 
     private lateinit var recyclerView: RecyclerView
     private lateinit var viewAdapter: RecyclerView.Adapter<*>
